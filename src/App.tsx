07b9ddc8be018bfe8079f48a9591c42e16074b7c
savmg <skbmasale941@gmail.com>
2023-12-09 05:34:09 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+import Home from "./_root/pages/Home";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+export default function App() {
+  return (
+    <main>
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
